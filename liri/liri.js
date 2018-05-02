ece4dcea9749eb0cf657689b1eef6398e9f73ecf
Jaylee Maruk <jmaruk@Jaylees-MBP.socal.rr.com>
2018-05-01 22:30:33 -0700

    add more

--- liri/liri.js
+++ liri/liri.js
@@ -18,18 +18,17 @@ if(task === 'my-tweets') {
     }
   });
 } else if ( task === 'spotify-this-song') {
-  console.log("spotify coming soon");
-} else {
-  console.log("I don't know what that means.");
-}
-
-
+  
 var spotify = new Spotify(keys.spotify);
- 
-spotify.search({ type: 'track', market: 'US', query: 'All the Small Things', limit: '1' }, function(err, data) {
+ var song = process.argv.slice(3);
+spotify.search({ type: 'track', market: 'US', query: song, limit: '1' }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
   }
  
-console.log(data.tracks.items); 
+console.log(data.tracks.items);   
 });
+} else {
+  console.log("I don't know what that means.");
+}
+
