7fcc649afa0b45ae5ddfb6699c49d5bf0cf57118
Jaylee Maruk <jmaruk@Jaylees-MacBook-Pro.local>
2018-05-01 18:31:49 -0700

    add

--- liri/liri.js
+++ liri/liri.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+console.log(Spotify);
 
 var client = new Twitter(keys.twitter);
 
@@ -20,3 +22,14 @@ if(task === 'my-tweets') {
 } else {
   console.log("I don't know what that means.");
 }
+
+
+var spotify = new Spotify(keys.spotify);
+ 
+spotify.search({ type: 'track', market: 'US', query: 'All the Small Things', limit: '1' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data.tracks.items); 
+});
